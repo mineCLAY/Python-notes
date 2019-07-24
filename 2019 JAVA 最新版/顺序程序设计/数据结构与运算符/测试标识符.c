6587989a2292fd65e36d7b3bb023bd59de4ac2ab
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-07-24 11:36:08 +0800

    Create 测试标识符.c

---
+++ 2019 JAVA 最新版/顺序程序设计/数据结构与运算符/测试标识符.c
@@ -0,0 +1,26 @@
+/*
+ * 测试标识符
+ */
+public class Main {
+  public static void main(String[] args) {
+
+    int a123 = 1;
+
+    // 数字不能开头
+    // int 123abc;
+
+    int $a = 3;
+    int _abc = 4;
+    // int #abc = 5;
+
+    /*
+     * 可以使用汉字
+     * 但不建议
+     */
+    int 年龄;
+
+    // 关键字不能作为标识符
+    // int class;
+
+    }
+}
