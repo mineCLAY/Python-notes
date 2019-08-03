6b30f7e00c7dcf2c68f2bccf1e89403c5a019211
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-08-03 12:10:55 +0800

    Create 冒泡排序.java

---
+++ 2019 JAVA 最新版/面向对象/数组/冒泡排序.java
@@ -0,0 +1,60 @@
+/*
+ * 冒泡排序
+ */
+
+// 导入包 java.util.Arrays 所有类
+import java.util.Arrays;
+
+public class test {
+
+  public static void main(String[] args) {
+
+    // 定义数组
+    int values[] ={ 3 , 1 , 6 , 2 , 9 , 0 , 7 , 4 , 5 , 8 };
+    // 定义变量
+    int temp = 0;
+
+    // 开始循环
+    for (int i = 0; i < values.length - 1; i++) {
+    
+      // 定义布尔变量
+      boolean flag = true;
+
+      // 嵌套循环
+      for (int j = 0; j < values.length - i - 1; j++) {
+
+        // 比较大小
+        if (values[j] > values[j + 1]) {
+
+          // 调换顺序
+          temp = values[j];
+          values[j] = values[j + 1];
+          values[j + 1] = temp;
+
+          // 给变量 flag 赋值
+          flag = false;
+
+        }
+
+        // 输出过程
+        System.out.println(Arrays.toString(values));
+
+      }
+
+      // 判断 flag 是否为 true
+      if (flag) {
+
+        // 输出
+        System.out.println("结束");
+
+        // 退出循环
+        break;
+
+      }
+
+    }
+
+    return;
+  }
+
+}
